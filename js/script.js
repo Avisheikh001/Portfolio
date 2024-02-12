b45a0c89f28c5ac3c975b3b28342f1f1f3b979cf
Avisheikh Kundu <153379289+Avisheikh001@users.noreply.github.com>
2024-02-12 09:42:56 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Programmer ","Python Developer","UI/UX designer",],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
